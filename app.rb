cd90d5d299306af0f9ef66d0432b4905b4e06dcf
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-24 21:51:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ module FormsLab
     post "/pirates" do 
       @pirate = Pirate.new(params["pirate"])
       
+      params[:pirate][:ship].each do |details|
+        Course.new(details)
+      end
+
       @ships = Ship.all
       erb :'pirates/show'
     end
