6ab42316efe3b69b753a040ea28a7ad689503a6d
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-24 21:46:59 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  SHIPS = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @weigth = params[:type]
+    @height = params[:booty]
+    SHIPS << self
+  end
+  
+  def self.all
+    SHIPS
+  end
+  
+  def self.clear
+    SHIPS.clear
+  end
+  
+  
 end
